b79b2a1b3d2de95c6472cfeb3c827f3eb96cd397
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:38:54 -0500

    Add files via upload

---
+++ Convertidor.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+type (5)
+print(type(5))
+print(type(8.5))
+print(type('Python'))
+print(type(False))
+print(type(False))
+print(type(8.5))
+print (8>5)
+print (8>=5)
+print (8==5)
+print (8!=5)
+print (8<5)
+# print (8<=5)
+x=5
+print(x,'\n'*2) #Saltos de linea
+print(x**5,'\n'*2)
+print(x/5,'\n'*2)
+print(x-5,'\n'*2)
+print(x+5,'\n'*2)
+print(x%5,'\n'*2)
+print(5//2,'\n'*2)
+
+
+
+
+
+
+
+
+
+
+
